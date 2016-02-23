4eadf40ea33204fe9b1bafe1abe5d20bbe24f8fd
Thao Truong <Thao_Truong@live.com>
2016-02-22 20:42:15 -0500

    fixed -1 err

--- front_end/create_transaction.cpp
+++ front_end/create_transaction.cpp
@@ -12,19 +12,19 @@ string CreateTransaction::Process(Users user) {
   string transaction_data = "";
 
   if (user.GetAccountName() != "") {   //if not admin
-    return -1;
+    return "invalid";
   }
   cout << "Account holder's name:" << endl;
   cin >> name;
 
   if (name.length() > 20 ) {    //20 character max length for name
-    return -1;
+    return "invalid";
   }
   cout << "Initial balance:" << endl;
   cin >> balance;
   
   if (atof(balance.c_str()) > 99999.99 ) {   //max initial balance is $99999.99
-    return -1;
+    return "invalid";
   }
   transaction_data = TransactionFile::WriteTransaction("create", name, "", "", ""); 
   return transaction_data;    
