e3279b3b6481b3756f7fc024261835625e3f82fb
Thao Truong <Thao_Truong@live.com>
2016-02-22 20:36:40 -0500

    fixed create trasnaction prototype error

--- front_end/create_transaction.cpp
+++ front_end/create_transaction.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-int CreateTransaction::Process(Users user) {
+string CreateTransaction::Process(Users user) {
   if (user.GetAccountName() != "") {   //if not admin
     return -1;
   }
