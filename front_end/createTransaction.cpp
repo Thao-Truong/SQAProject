15eefcfa76da114dfe7343b79bbaa427c021a260
Robert Kocovski <robert.kocovski@uoit.net>
2016-02-23 01:35:07 +0000

    Fixed create_transaction.cpp

--- front_end/createTransaction.cpp
+++ front_end/createTransaction.cpp
@@ -9,24 +9,24 @@ using namespace std;
 
 class createTransaction {
   private:
-    string name, accountNumber, balance;
+    string name, account_number, balance;
 
   public:
-    int process(Users user) {  
+    string Process(Users user) {  
            
-    if (user.getAccountName() != "") {   //if not admin
-      return -1;
+    if (user.GetAccountName() != "") {   //if not admin
+      cout << "Transaction invalid." << endl;
     }
     cout << "Account holder's name:" << endl;
     cin >> name;
 
     if (name.length() > 20 ) {    //20 character max length for name
-      return -1;
+      cout << "Transaction invalid." << endl;
     }
     cout << "Initial balance:" << endl;
     cin >> balance;
     if (atof(balance.c_str()) > 99999.99 ) {   //max initial balance is $99999.99
-      return -1;
+      cout << "Transaction invalid." << endl;
     }
   }
 };
