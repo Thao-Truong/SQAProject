bac15f7c3139c3214690b9f9741fc2e9d924a703
Thao Truong <Thao_Truong@live.com>
2016-02-22 21:15:00 -0500

    pt lt err

--- front_end/front_end.cpp
+++ front_end/front_end.cpp
@@ -75,7 +75,7 @@ void FrontEnd::GetTransactions() {
     } else if (input.compare("paybill") == 0 && flag == true) {
         PaybillTransaction pt;
         Users* all_users = lt.GetUsers();
-        current_transactions.push_back(tt.Process(user, all_users));
+        current_transactions.push_back(pt.Process(user, all_users));
 
         if (current_transactions.back().compare("invalid") == 0) {
           cout << "Transaction invalid." << endl;
