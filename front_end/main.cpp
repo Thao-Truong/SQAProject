ff722e028100907e2fa376c0c68201dfe5e7fd74
Thao Truong <Thao_Truong@live.com>
2016-03-04 12:48:37 -0500

    added makefile and arguments for main.cpp

--- front_end/main.cpp
+++ front_end/main.cpp
@@ -1,7 +1,16 @@
 #include "front_end.h"
+#include <iostream>
+using namespace std; 
 
-int main() {
-   FrontEnd myFrontEnd;
-   myFrontEnd.GetTransactions();
-   return 0;
+int main(int argc, char* argv[]) {
+  if (argc == 3) {
+  	cout << "accounts file: " << argv[1] << endl;
+  	cout << "transaction file: " << argv[2] << endl;
+
+  	FrontEnd myFrontEnd;
+  	myFrontEnd.GetTransactions();
+  } else {
+  	cout << "Invalid Arguments" << endl;
+  }  
+  return 0;
 }
