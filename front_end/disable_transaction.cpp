f35979873f157e3d874a56d02aaecb2c8f2cc0e3
Robert Kocovski <robert.kocovski@uoit.net>
2016-03-07 08:29:31 +0000

    small change

--- front_end/disable_transaction.cpp
+++ front_end/disable_transaction.cpp
@@ -20,7 +20,7 @@ using namespace std;
 // Enables a disabled account and writes the transaction line
 string DisableTransaction::Process(Users user, Users* all_users) {
   string transaction_data = "";
-
+  string acct_number;
   if (user.GetAccountName() == "") {      //admin login
     cout << "Account holder's name:" << endl;
     cin.ignore();
@@ -36,8 +36,12 @@ string DisableTransaction::Process(Users user, Users* all_users) {
   for (int i = 0; i < 10; i++) {   // search for status of account holder
     if (all_users[i].GetAccountName().compare(name) == 0) {
       status = all_users[i].GetStatus();
+      acct_number = all_users[i].GetAccountNumber();
       break;
     }
+  }
+  if (acct_number != account_number) {
+    return "invalid";
   }
   if (status.compare("A") == 0 ) {
     // we only write a line to the transaction file (the accounts file will be changed in the back-end)
