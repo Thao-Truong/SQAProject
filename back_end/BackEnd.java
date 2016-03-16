5baff4f3246c6ce2bd6a621036783e57ced8af01
Thao Truong <Thao_Truong@live.com>
2016-03-16 16:38:53 -0400

    Skeleton for each transaction, implemented transactions class and calling of transactions

--- back_end/BackEnd.java
+++ back_end/BackEnd.java
@@ -1,23 +1,30 @@
 import java.io.*;
 
-// Processes the transaction files and user files
+/* Processes the transaction files and user files */
 public class BackEnd {
   private String transactionFile = "transactions";
   private String userFile = "current_accounts";
 
-  // Constructor
+  /* Constructor for BackEnd class that calls the appropriate method to begin processing the trasactions*/
   public BackEnd() {
     processTransactions();
   }
 
-  // Constructor
+  /* 
+  * Constructor for BackEnd class that takes in the file names and stores them
+  * @param transactionFile - the name of the transaction file to use
+  * @param userFile - the name of the user accounts file to use
+  */
   public BackEnd(String transactionFile, String userFile) {
     this.transactionFile = transactionFile;
     this.userFile = userFile;
   }
 
 
-  // Processes transactions
+  /* 
+  * Calls the appropriate classes to read in the files
+  * Goes through each transaction and calls the appropriate actions
+   */
   public void processTransactions() {
   	FileReader transactionsReader = getFile(transactionFile);
   	// UserAccounts userAccounts = new UserAccounts(userFile);
@@ -33,20 +40,118 @@ public class BackEnd {
 
 	  		Transaction transaction = new Transaction(currentTransaction);
 
-	  		// if (transaction.getCode() == "00") {
-	  		// 	//process transaction
-	  		// } 
+	  		switch(transaction.getCode()) {
+	  			case "01":
+	  				System.out.println(transaction.getCode());
+	  				withdrawal();
+	  				break;
+	  			case "02":
+	  				System.out.println(transaction.getCode());
+	  				transfer();
+	  				break;
+	  			case "03":
+	  				System.out.println(transaction.getCode());
+	  				paybill();
+	  				break;
+	  			case "04":
+	  				System.out.println(transaction.getCode());
+	  				deposit();
+	  				break;
+	  			case "05":
+	  				System.out.println(transaction.getCode());
+	  				create();
+	  				break;
+	  			case "06":
+	  				System.out.println(transaction.getCode());
+	  				delete();
+	  				break;
+	  			case "07":
+	  				System.out.println(transaction.getCode());
+	  				disable();
+	  				break;
+  				case "08":
+	  				System.out.println(transaction.getCode());
+	  				changePlan();
+	  				break;
+	  			case "09":
+	  				System.out.println(transaction.getCode());
+	  				enable();
+	  				break;
+	  			case "10":
+	  				System.out.println(transaction.getCode());
+	  				login();
+	  				break;
+	  			case "00":
+	  				System.out.println(transaction.getCode());
+	  				endOfSession();
+	  				break;
+	  			default:
+	  				System.out.println("Invalid input");
+	  		}
 
-
-	  		System.out.println(currentTransaction);
 	  	}
   	} catch (Exception E) {
-  		System.out.println("err");
+  		System.out.println("Error");
   	}
   }
 
+  /*  */
+  private void withdrawal() {
+
+  }
+
+  /* */
+  private void transfer() {
+
+  }
+
+  /* */
+  private void paybill() {
+
+  }
+
+  /* */
+  private void deposit() {
+
+  }
+
+  /* */
+  private void create() {
+
+  }
+
+  /* */
+  private void delete() {
+
+  }
+
+  /* */
+  private void disable() {
+
+  }
+
+  /* */
+  private void changePlan() {
+
+  }
+
+  /* */
+  private void enable() {
+
+  }
+
+  /* */
+  private void login() {
+
+  }
+
+  /* Last transaction */ 
+  private void endOfSession() {
+
+  }
+
 	
-  // Open or initialize files
+  /* Open or initialize files */
   public FileReader getFile(String fileName) {
     // Check if file exists
     FileReader fr = null;
@@ -68,6 +173,7 @@ public class BackEnd {
     return fr;
   }
 
+  /* Initializes Backend to begin processing the transactions*/
   public static void main(String[] args) {
     BackEnd backEnd = new BackEnd();
   }
--- back_end/Transaction.java
+++ back_end/Transaction.java
@@ -1,16 +1,52 @@
 import java.io.*;
 
+/* 
+* Transaction class keeps track of each individual transaction
+* and the actions in regards to it
+*/
 public class Transaction { 
 	private String code;
 	private String accountName;
 	private String accountNumber;
-	 
-	// Constructor
+	private String accountBalance;
+	private String misc;
+
+	/*
+	* Constructor for transaction class that takes a transaction line and 
+	* stores them to the appropriate variable
+	*
+	* @param transactionLine - A single line from the transaction file
+	*/
   public Transaction (String transactionLine){
-  	
+  	code = transactionLine.substring(0, 2);
+  	accountName = transactionLine.substring(3, 13);
+  	accountNumber = transactionLine.substring(25, 29);
+  	accountBalance = transactionLine.substring(30, 38);
+  	misc = transactionLine.substring(39, 41);
   }
 
-  public void getCode(){
+  /* @return code - returns the transaction's code */
+  public String getCode(){
+  	return code;
+  }
 
+ /* @return accountName - returns the accountName that corresponds to the transaction */
+  public String getName(){
+  	return accountName;
+  }
+
+ /* @return accountNumber - returns the accountNumber that corresponds to the transaction */
+  public String getNumber(){
+  	return accountNumber;
+  }
+
+ /* @return accountBalance - returns the accountBalance that corresponds to the transaction */
+  public String getBalance(){
+  	return accountBalance;
+  }
+
+ /* @return accountMisc - returns the miscallaneous data that corresponds to the transaction */
+  public String getMisc(){
+  	return misc;
   }
 }
