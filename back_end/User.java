5feb0566fc48ecde098f52dbbd005ebadb9a14d0
Robert Kocovski <robert.kocovski@uoit.net>
2016-03-18 02:09:09 +0000

    Finished commenting

--- back_end/User.java
+++ back_end/User.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-// creates a User object that stores info about each user 
+/* User class stores info about each user's account */
 public class User {
   
   private String userName;
@@ -9,11 +9,14 @@ public class User {
   private String balance;
   private String plan;
 
+/* no argument constructor for User class */
   public User() {
     
   }
-  // Constructor
-  // Read in line and parse it to store appropriate data
+  /* 
+  * Constructor for User class 
+  * @param userLine - string to hold each user's info
+  */
   public User(String userLine) {
     accountNumber = userLine.substring(0, 4);
     userName = userLine.substring(6, 25);
@@ -21,48 +24,73 @@ public class User {
     balance = userLine.substring(29, 36);
     plan = userLine.substring(38, 38);
   }
-  
+
+  /* Get method that returns user's name */
   public String getUserName() {
     return userName;
   }
 
+  /* Get method that returns user's account number */
   public String getAccountNumber() {
     return accountNumber;
   }
 
+  /* Get method that returns the status of the account (A = active; D = disabled) */
   public String getStatus() {
     return status;
   }
-  
+
+  /* Get method that returns the user's account balance */
   public String getBalance() {
     return balance;
   }
-  
+
+  /* Get method that returns the user's plan type (S = student; N = non-student) */
   public String getPlan() {
      return plan;
   }
-  
+
+  /* 
+  * Sets the user's name
+	* @param userName - user's name
+  */
   public void setUserName(String userName) {
     this.userName = userName;
   }
-  
+
+  /* 
+  * Sets user's account number
+	* @param accountNumber - user's account number
+  */
   public void setAccountNumber(String accountNumber) {
     this.accountNumber = accountNumber;
   }
-  
+
+  /* 
+  * Sets the status of user's account (A = active; D = disabled) 
+	* @param status - user's account status
+  */
   public void setStatus(String status) {
     this.status = status;
   }
 
+  /* 
+  * Sets the user's account balance
+	* @param balance - the user's account balance
+  */
   public void setBalance(String balance) {
     this.balance = balance;
   }
-  
+
+  /* 
+  * Sets the account plan type  (S = student; N = non-student)
+	* @param plan - user's account plan type
+  */ 
   public void setPlan(String plan) {
     this.plan = plan;
   }
   
-  // Format this user data to be written to file
+  /* Formats user's data to be written to file */
   public String userAccountOuput() {
     return accountNumber + " " + userName + " " + status + " " + balance + " " + plan;
 
--- back_end/UserAccounts.java
+++ back_end/UserAccounts.java
@@ -1,19 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-// store all user accounts
+/* UserAccounts class stores all user accounts */
 public class UserAccounts {
   
   private ArrayList<User> userAccounts;
 
+  /* No argument constructor */
   public UserAccounts() {
     userAccounts = new ArrayList<User>(); 
   }
+
+  /* 
+  * Constructor for UserAccounts class
+  * @param fileName - name of accounts file
+  */
   public UserAccounts(String fileName) {
-    //read file
-    //for each line, make a user and add to userAccounts 
-    
-    // check if file exists; if it doesn't create.
     try {
       File file = new File(fileName);
       if (!file.exists()) {
@@ -25,9 +27,9 @@ public class UserAccounts {
       BufferedReader br = new BufferedReader(fileReader);
       String accountLine;
 
-      // Read in each Account line
+      /* Read in each Account line */
       while ((accountLine = br.readLine()) != null) {
-        // Get Account Data
+        /* Get account data */
         String currentAccount = accountLine;
         User user = new User(currentAccount);
         if (user.getUserName() == "END_OF_FILE         ") {
@@ -39,28 +41,46 @@ public class UserAccounts {
 
     } catch (FileNotFoundException e) {
       	
-      	// Err file not found message
+      /* Error file not found message */
     } catch(IOException e) {
-      	// Err Reading file
+      /* Error Reading file */
     }
-  }  // end constructor  
-  
+  }  
+  /* 
+  * Gets a User object at a given index
+  * @param index - index in the arraylist
+  */
   public User getUser(int index) {
     return userAccounts.get(index);
   }
-
+  
+  /* 
+  * Adds a User object to arraylist
+  * @param user - User object to add to arraylist
+  */
   public void addUser(User user) {
     userAccounts.add(user);
   }
 
+  /* 
+  * Removes a User object from arraylist
+  * @param index - index of arraylist
+  */
   public void removeUser(int index) {
     userAccounts.remove(index);
   }
   
+  /* 
+  * Number of User objects in the arraylist
+  */
   public int userCount() {
     return userAccounts.size();
   }
  
+  /* 
+  * Gets index of User object that contains given account number
+  * @param accountNumber - user's account number
+  */
   public int getIndex(String accountNumber) {
     int result = -1;
     for (int i = 0; i < userAccounts.size(); i++) {
@@ -71,10 +91,19 @@ public class UserAccounts {
     return result;
   }
 
+  /* 
+  * Updates User object stored at given index in arraylist
+  * @param index - index of arraylist
+  * @param currentUser - User object with updated info
+  */
   public void updateUser(int index, User currentUser) {
     userAccounts.set(index, currentUser);
   }
 
+  /* 
+  * Writes all the accounts stored in the arraylist to file
+  * @param fileName - name of file to write accounts to
+  */
   public void writeAccounts(String fileName) {
     try {
       FileWriter fileWriter = new FileWriter(fileName);
@@ -88,4 +117,4 @@ public class UserAccounts {
         System.out.println("Error writing to file '" + fileName + "'");
     }
   }
-} // end class UserAccounts
+} 
