a1bc3717a5d6bf7fc5ed8ddd010ada73db89bad5
Thao Truong <Thao_Truong@live.com>
2016-03-23 01:02:36 -0400

    generated BackEndTest start

---
+++ back_end/BackEndTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BackEndTest {
+
+	@Test
+	public void testBackEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBackEndStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
