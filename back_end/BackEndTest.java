60d81a3d60aec7ee291203496f6aafa15cef8c12
Thao Truong <Thao_Truong@live.com>
2016-03-29 16:43:33 -0400

    changed skeleton structure to include lal methods that need to be tested

--- back_end/BackEndTest.java
+++ back_end/BackEndTest.java
@@ -5,28 +5,112 @@ import org.junit.Test;
 public class BackEndTest {
 
 	@Test
-	public void testBackEnd() {
-		fail("Not yet implemented");
-	}
+    public void testBackEndEmpty() {
+        fail("Not yet implemented");
+    }
 
 	@Test
-	public void testBackEndStringString() {
-		fail("Not yet implemented");
-	}
+    public void testBackEnd() {
+        fail("Not yet implemented");
+    }
 
 	@Test
-	public void testProcessTransactions() {
-		fail("Not yet implemented");
-	}
+    public void testWithdrawalStudent() {
+        fail("Not yet implemented");
+    }
+
 
 	@Test
-	public void testGetFile() {
-		fail("Not yet implemented");
-	}
+    public void testWithdrawalNonStudent() {
+        fail("Not yet implemented");
+    }
 
 	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
+    public void testTransferStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testTransferNonStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testPaybillStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testPaybillNonStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void tesDepositStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testDepositNonStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testCreate() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testDelete() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testDisable() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testChangePlanStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testChangePlanNonStudent() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testEnable() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testLoginAdmin() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testLoginStandard() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testEndOfSession() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testGerneatedNumberLength() {
+        fail("Not yet implemented");
+    }
+
+	@Test
+    public void testGeneratedNumberDoesNotExist() {
+        fail("Not yet implemented");
+    }
+
+
+
 
 }
